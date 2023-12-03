8ce301bcbb0c19e0a7d374eba92012b4dfcee530
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2023-12-03 12:45:08 +0000

    routes.rb　記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
-class ApplicationController < ActionController::
+class ApplicationController < ActionController::Base  
   before_action :authenticate_user!, except: [:top]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,26 @@
 Rails.application.routes.draw do
-  
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
   # 顧客用
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  # 管理者用
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-  
+
   namespace :admin do
-     root to: 'homes#top'
+    root to: 'homes#top'
     resources :skinitems, only: [:index, :new, :show, :edit]
   end
-  namespace :public do
-    root to:  'public/homes#top'
-    get 'about' => 'homes#about'
-    get 'users/index'
-    get 'users/check'
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :public do
+
+
+  root to:  'public/homes#top'
+  get 'about' => 'homes#about'
+  resources :posts, only: [:index, :new, :show,:edit]
+  resource :users
     
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/show'
-    get 'posts/edit'
-  end
+    
 end
