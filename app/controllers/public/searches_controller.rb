1e3489bdf65acb93c8db70c395ffc8bf5efbd0cb
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2024-01-23 13:15:48 +0000

    リファクタリング

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,13 +3,15 @@ class Public::SearchesController < ApplicationController
 
   # 検索を実行し、結果をインスタンス変数に格納
   def search
+    
     @range = params[:range]   # 検索対象の範囲を取得
     @word = params[:word]   # 検索キーワードを取得
 
     if @range == "ユーザー"   # 範囲がユーザーの場合
       @users = User.looks(params[:search], params[:word])   # ユーザーモデルのlooksメソッドを呼び出し、検索結果を取得
     else
-      @posts = Post.looks(params[:search], params[:word], params[:skin_type])   # ポストモデルのlooksメソッドを呼び出し、検索結果を取得
+      @posts = Post.looks(params[:search], params[:word], params[:skin_type]) 
+      # ポストモデルのlooksメソッドを呼び出し、検索結果を取得
     end
   end
 end
